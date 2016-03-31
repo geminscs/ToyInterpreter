f25a74e8e5e30ebedf1bd9e21565614f3d29af7d
admin <admin@pc-a006.local>
2016-03-31 18:44:23 +0900

    Initial Commit

---
+++ ToyInterpreter/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ToyInterpreter
+//
+//  Created by admin on 3/31/16.
+//  Copyright © 2016 admin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
